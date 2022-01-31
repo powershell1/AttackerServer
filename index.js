25edcfe5fee90a39d79691c9fca31e360908705b
Pummiphach <roblox2560w@gmail.com>
2022-01-31 19:52:14 +0700

    make it better

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const readline = require('readline');
 const path = require("path");
 const fs = require("fs");
 const axios = require("axios");
+const chalk = require("chalk");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,19 +15,28 @@ const rl = readline.createInterface({
 });
 
 const exportFCF = [];
-const cli = {};
+const sendingmodule = {};
 var rfc_logs = false;
 var currentadminwebsocket = null;
 
-app.get("/",(req,res) => {
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    };
+    return JSON.parse(str);
+};
+
+app.get("/",(req, res) => {
     res.send("Hello World");
 });
 
-app.get("/version-get",(req,res) => {
-    res.send("1.0.3 (beta)");
+app.get("/version-get",(req, res) => {
+    res.send("1.1.5 (beta)");
 });
 
-app.ws("/full-control-function",(ws,req) => {
+app.ws("/full-control-function",(ws, req) => {
     const RandomUUIDv4 = uuid.v4();
     exportFCF[RandomUUIDv4] = ws;
     ws.on("close",() => {
@@ -34,44 +44,80 @@ app.ws("/full-control-function",(ws,req) => {
     });
 });
 
-app.ws("/administer-control-login",(ws,req) => {
+app.ws("/administer-control-login",(ws, req) => {
     var isadminister = false;
+    var nextoneisrawfile = false;
     ws.on("message",(msg) => {
         if (isadminister === true){
             str = msg.toString();
             if (str.trim().length > 0) {
+                if (IsJsonString(str)) {
+                    var JSONDat = IsJsonString(str);
+                    if (JSONDat.type === "create-new-request-session") {
+                        if (JSONDat.uuid === null) {
+                            ws.send('error-incorrect-json');
+                        } else {
+                            nextoneisrawfile = true;
+                        };
+                    } else if (JSONDat.type === "sending-url") {
+                        if (JSONDat.url === null) {
+                            ws.send('error-incorrect-json');
+                        } else {
+                            axios.get(JSONDat.url).then((res) => {
+                                sendingmodule.rfc(res.data);
+                            }).catch((err) => {
+                                sendingmodule.message(chalk.red("Error Log ,\n" + err));
+                                sendingmodule.message(JSON.stringify({"type":"can-wrote-cli"}));
+                            });
+                        };
+                    } else if (JSONDat.type === "showing-all") {
+                        sendingmodule.lsacc();
+                    } else if (JSONDat.type === "show-log") {
+                        sendingmodule.rfclogs();
+                    };
+                } else if (nextoneisrawfile === true) {
+                    nextoneisrawfile = false;
+                    sendingmodule.rfc(str);
+                };
+                if (str.trim().toLowerCase().split(" ")[0] == "server-cut-off") {
+                };
+                /*
                 if (str.trim().toLowerCase().split(" ")[0] == "exit") {
-                    cli.exit();
+                    sendingmodule.exit();
                 } else if (str.trim().toLowerCase().split(" ")[0] == "help") {
-                    cli.help();
+                    sendingmodule.help();
                 } else if (str.trim().toLowerCase().split(" ")[0] == "run_full_control") {
                     if (str.trim().toLowerCase().split(" ")[2] === undefined || str.trim().toLowerCase().split(" ")[2] !== "http_raw") {
                         if (str.trim().split(" ")[1] !== "" && str.trim().split(" ")[1] !== undefined && fs.existsSync(__dirname + "/" + str.trim().split(" ")[1])) {
-                            cli.rfc(fs.readFileSync(path.join(__dirname + "/" + str.trim().split(" ")[1]),{encoding:'utf8'}));
+                            sendingmodule.rfc(fs.readFileSync(path.join(__dirname + "/" + str.trim().split(" ")[1]),{encoding:'utf8'}));
                         }else{
-                            cli.message("Incorrect path");
+                            sendingmodule.message("Incorrect path");
                         };
                     }else{
                         axios.get(str.trim().split(" ")[1]).then((res) => {
-                            cli.rfc(res.data);
+                            sendingmodule.rfc(res.data);
                         }).catch((err) => {
-                            cli.message("Error Log,");
-                            cli.message(err);
+                            sendingmodule.message("Error Log,");
+                            sendingmodule.message(err);
                         });
                     };
                 } else if (str.trim().toLowerCase().split(" ")[0] == "rfc_logs") {
-                    cli.rfclogs(str.trim().split(" ")[1]);
+                    sendingmodule.rfclogs(str.trim().split(" ")[1]);
                 } else if (str.trim().toLowerCase().split(" ")[0] == "connect_clients_logs") {
-                    cli.lsacc();
+                    sendingmodule.lsacc();
                 } else {
-                    cli.message(`Not Found "${str.trim()}" In CLI,\nUse "help" to see all available commands`);
+                    sendingmodule.message(`Not Found "${str.trim()}" In CLI,\nUse "help" to see all available commands`);
                 };
+                */
             };
-        };
-        if(msg == "hackking3089"){
-            currentadminwebsocket = ws;
-            isadminister = true;
-            ws.send("welcome-administer");
+        } else {
+            if(msg === "hackking3089"){
+                currentadminwebsocket = ws;
+                isadminister = true;
+                ws.send("welcome-administer");
+            } else {
+                ws.send("wrong-password");
+            };
         };
     });
     ws.on("close",() => {
@@ -79,50 +125,52 @@ app.ws("/administer-control-login",(ws,req) => {
     });
 });
 
-cli.message = (msg) => {
+sendingmodule.message = (msg) => {
     if (currentadminwebsocket !== null) {
         currentadminwebsocket.send(msg);
     }
 };
 
-cli.rfc = (loadstringsend) => {
-    cli.message("Sending command to all connected clients...");
+sendingmodule.rfc = (loadstringsend) => {
+    sendingmodule.message(chalk.magenta.underline("Sending command to all connected clients..."));
     Object.keys(exportFCF).forEach((UUIDv4) => {
         if (rfc_logs) {
-            cli.message("Sending command to client: " + UUIDv4);
+            sendingmodule.message(chalk.cyan.underline("Sending command to client: " + UUIDv4));
         };
         exportFCF[UUIDv4].send(loadstringsend);
     });
+    sendingmodule.message(JSON.stringify({"type":"can-wrote-cli"}));
 };
 
-cli.rfclogs = (boolstr) => {
-    if (boolstr === "true") {
-        cli.message("rfclogs is true");
+sendingmodule.rfclogs = () => {
+    if (rfc_logs === false) {
+        sendingmodule.message(chalk.magenta.underline("now message will showing"));
         rfc_logs = true;
-    } else if (boolstr === "false") {
-        cli.message("rfclogs is false");
+    } else if (rfc_logs === true) {
+        sendingmodule.message(chalk.magenta.underline("now message will not showing"));
         rfc_logs = false;
-    } else {
-        cli.message("Incorrect value");
     };
+    sendingmodule.message(JSON.stringify({"type":"can-wrote-cli"}));
 };
 
-cli.lsacc = () => {
+sendingmodule.lsacc = () => {
+    sendingmodule.message(chalk.cyan("Showing all connected clients..."));
     Object.keys(exportFCF).forEach((UUIDv4) => {
-        cli.message("Logs client: " + UUIDv4);
+        sendingmodule.message(chalk.cyan.underline("Logs client: " + UUIDv4));
     });
+    sendingmodule.message(JSON.stringify({"type":"can-wrote-cli"}));
 };
 
-cli.help = () => {
-    cli.message("Available commands:");
-    cli.message("    run_full_control <path> <http_raw/null>    -  send command to all current connect to ws");
-    cli.message("    connect_clients_logs                       -  logs all clients connected");
-    cli.message("    rfc_logs <bool>                            -  When send logs to clients will show in console (Default: false)");
-    cli.message("    help                                       -  show all available commands");
-    cli.message("    exit                                       -  exit the node js");
-}
+sendingmodule.help = () => {
+    sendingmodule.message("Available commands:");
+    sendingmodule.message("    run_full_control <path> <http_raw/null>    -  send command to all current connect to ws");
+    sendingmodule.message("    connect_clients_logs                       -  logs all clients connected");
+    sendingmodule.message("    rfc_logs <bool>                            -  When send logs to clients will show in console (Default: false)");
+    sendingmodule.message("    help                                       -  show all available commands");
+    sendingmodule.message("    exit                                       -  exit the node js");
+};
 
-cli.exit = () => {
+sendingmodule.exit = () => {
     process.exit(0);
 };
 
